e6a098d47860e1c6e8a8d1a4d71983ddf49642c7
Beau Albiston <beauzo@hotmail.com>
2024-01-23 23:04:35 -0600

    Move LCA deepest leaves to root

--- lca_deepest_leaves.py
+++ lca_deepest_leaves.py
@@ -1,11 +1,5 @@
 from typing import Optional
-
-
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+import tree_node
 
 
 def array_to_binary_tree(array: list) -> TreeNode:
